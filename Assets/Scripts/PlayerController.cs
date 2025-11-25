f30ae2864541710d18136c2363ece8bc7f4ce135
Dasha-kykyrys <dashulya.art@gmail.com>
2025-11-26 02:06:48 +0300

    Настройка использования анимаций игрока

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,17 +10,40 @@ public class Movement : MonoBehaviour
     private Rigidbody2D rigidbody2d;
     private Vector2 movement;
 
+    // Анимация персонажа
+    public Animator anim;
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Получение направление передвижения
+        movement = new Vector2(joystick.Horizontal, joystick.Vertical);
+
+        // Обновление анимации
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
     {
-        // Определение направления
-        movement = new Vector2(joystick.Horizontal, joystick.Vertical);
-
         // Перемещение персонажа
         rigidbody2d.MovePosition(rigidbody2d.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void UpdateAnimation()
+    {
+        // Определение направления персонажа
+        if (movement.magnitude > 0.1f) 
+        {
+            anim.SetFloat("directionX", movement.x);
+            anim.SetFloat("directionY", movement.y);
+        }
+
+        // Определение движется ли персонаж
+        anim.SetFloat("speed", movement.magnitude);
     }
 }
